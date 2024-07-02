b17a8081fccd0af1899048648ddc70c37cdea22a
Izzat <izzatillabdurasulovv@gmail.com>
2024-07-02 14:28:15 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+// let client = {
+//   age: 20, // number
+//   nationality: "uzbek", // string
+//   isApproved: false, // boolean
+// };
+
+// console.log(client);
+
+// let number = 6;
+// console.log(8 / 0); // infinity
+// console.log("izzat" * 5); // NaN => not a number
+
+("use strict");
+
+// alert ("hello javascript")
+// const place = confirm("are you here?")
+// console.log(place);
+
+// const currrentPlace = prompt("where are you?", "")
+// console.log(typeof currrentPlace);
+
+// const age = +prompt("how old are you?");
+// console.log(typeof age );
+
+// const selectedColor = [];
+
+// selectedColor[0] = prompt("what's your selected color #1", "");
+// selectedColor[1] = prompt("what's your selected color #2", "");
+// selectedColor[2] = prompt("what's your selected color #3", "");
+
+// console.log(selectedColor);
+
+// const channel = prompt("what's your favourite youtuber")
+// console.log("https://youtube.com/" + channel + "/videos");
+
+// const channel = prompt("what's your favourite youtuber")
+// console.log("https://youtube.com/${channel}");
+
+// let incr = 10;
+// let decr = 10;
+
+// incr++;
+// decr--;
+// console.log(incr);  
+// console.log(decr);
+
+// console.log(5*5 === "25"); // faqat qiymatni to'g'riligini ko'rsatadi.
+// console.log(5*5 == "25"); // ham qiymat ham ma'lumot turini tekshiradi.
+
+
+// && = va , || = yoki
+
+// const isAge = true;
+// const isClose = false;
+// const isChecked = true;
+// console.log(isAge && isClose && !isChecked);
